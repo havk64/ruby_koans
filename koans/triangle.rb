347701276c2b2500e04d5d7ac8a443c31618fcd5
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-02-23 21:44:41 -0300

    Triangle exceptions completed

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if (a <= 0 || b <= 0 || c <= 0) || a+b <= c || b+c <= a || a+c <= b
+    raise TriangleError
+  end
+
   if (a + b + c)/3 == a && a == b && b == c
       return :equilateral
   elsif a == b || b == c || a == c
